0a957ff1bf272b69e05f3fdb328c6d00c8d0b8a0
Anna Panova <annapanovadev@gmail.com>
2024-06-04 20:18:30 +0200

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Anna Panova on 04.06.24.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
