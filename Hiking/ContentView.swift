01d43ff0f56e1082d59b76bef78a4f049519648f
Anna Panova <annapanovadev@gmail.com>
2024-06-04 20:50:42 +0200

    Added Model for Hike, hardcoded list of hikes in ContentView.

--- Hiking/ContentView.swift
+++ Hiking/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        // list of hikes:
+        let hikes = [
+            Hike(name: "Salmonberry Trail", photo: "sal", miles: 6),
+            Hike(name: "Tom, Dick, and Harry Mountain", photo: "tom", miles: 5.8),
+            Hike(name: "Tamanawas Falls", photo: "tam", miles: 5)
+        ]
+        
+        List(hikes) { hike in
+            HStack{
+                Text(hike.name)
+            }
         }
-        .padding()
+        
     }
 }
 
---
+++ Hiking/Models/Hike.swift
@@ -0,0 +1,22 @@
+//
+//  Hike.swift
+//  Hiking
+//
+//  Created by Anna Panova on 04.06.24.
+//
+
+import Foundation
+
+
+// sigle hike
+
+struct Hike: Identifiable {
+    
+    // to confirm Identifiable Hike has to have unic identifier:
+    let id = UUID()
+    
+    // description of hike:
+    let name: String
+    let photo: String
+    let miles: Double
+}
